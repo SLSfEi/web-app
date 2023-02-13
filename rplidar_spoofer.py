3bc8986705bff2359781d8ada7df12edb0237886
Phodi <5670253+Phodi@users.noreply.github.com>
2023-02-13 20:22:48 +0700

    init

---
+++ rplidar_spoofer.py
@@ -0,0 +1,29 @@
+import requests, time, random, argparse
+SERVER_ENDPOINT = "http://127.0.0.1:8000/api/v1/scan"
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--once", "-o", action="store_true", help="send once")
+args = arg_parser.parse_args()
+
+def gen_data():
+    data = ["start_flag,angle,distance,quality"]
+    cur_angle = 0
+    rand_inc = random.random()
+    while(cur_angle + rand_inc <= 360):
+        cur_angle += rand_inc
+        data.append(f"1,{cur_angle:.3f},999,999")
+        rand_inc = random.random()
+    return "\n".join(data)
+
+def send_request():
+    x = requests.post(SERVER_ENDPOINT,data=gen_data(),headers={"Content-Type": "text/csv"})
+    print(x.text)
+
+if(__name__ == "__main__"):
+    if (args.once):
+        send_request()
+    else:
+        while(True):
+            send_request()
+            time.sleep(0.3)
+
---
+++ smart_rplidar/radar_screen/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, JsonResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+import radar_screen.views_code.rplidar as rplidar
+from .models import ScanIteration
+
+from smart_rplidar.settings import BASE_DIR
+
+from background_task import background
+
+TEMPLATE_DIR =  BASE_DIR / "radar_screen" / "templates"
+
+def home(req):
+    #Basic template render
+    template = loader.get_template(TEMPLATE_DIR / "home.html")
+    context = {}
+    return HttpResponse(template.render(context, req))
+
+def radar(req):
+    #[TODO: Research on how to make ws tick work or something]
+    scan_iter = ScanIteration.objects.filter(id=0).first()
+    if scan_iter is None:
+        context = {
+            "scan_timestamp": "NODATA",
+            "scan_csv": "NODATA"
+        }
+    else:
+        scan_timestamp = scan_iter.timestamp
+        scan_csv = scan_iter.csv_string
+        context = {
+            "scan_timestamp": scan_timestamp,
+            "scan_csv": scan_csv
+        }
+
+    template = loader.get_template(TEMPLATE_DIR / "radar.html")
+    return HttpResponse(template.render(context, req))
+
+@csrf_exempt
+def scan(req):
+    #rplidar endpoints
+    if req.method == "POST":
+        return rplidar.update_scan(req)
+    elif req.method == "GET":
+        return rplidar.get_scan(req)
+    else:
+        return Http404()
