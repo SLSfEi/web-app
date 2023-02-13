0468dc7982953fa778030c45588f47d4d2e96326
Phodi <5670253+Phodi@users.noreply.github.com>
2023-02-13 21:43:11 +0700

    remove old project

--- smart_rplidar_bak/radar_screen/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class ScanIteration(models.Model):
-    id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    csv_string = models.TextField(null=True)
