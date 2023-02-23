43998fa27a7b2a69a73bd400800d2a1ff4fa7bf7
Phodi <5670253+Phodi@users.noreply.github.com>
2023-02-23 17:11:47 +0700

    basic html layout

--- smart_rplidar/radar_screen/static/js/radar.js
+++ smart_rplidar/radar_screen/static/js/radar.js
@@ -1,5 +1,9 @@
-const app = new PIXI.Application({ width: 1000, height: 1000 , background: '#1099bb' });
-const canvas_host = document.getElementById("canvas-host");
+const app = new PIXI.Application(
+    {
+        transparent: false,
+        background: '#FFFFFF'
+    });
+const canvas_host = document.getElementById("radar-screen");
 canvas_host.appendChild(app.view);
 
 const points_container = new PIXI.Container();
@@ -8,20 +12,16 @@ app.stage.addChild(points_container);
 // Create a 5x5 grid of points
 for (let i = 0; i < 25; i++) {
     const point = new PIXI.Graphics();
-    point.beginFill(0xFFFF00);
-    point.drawCircle(0,0,2);
-    point.endFill();
+    point.beginFill(0x000000)
+    .drawCircle(0,0,2)
+    .endFill();
     point.x = (i % 5) * 40;
     point.y = Math.floor(i / 5) * 40;
     points_container.addChild(point);
 }
 
-var hello = () => {
-    console.log("hello?")
-}
+points_container.x = app.screen.width / 2;
+points_container.y = app.screen.height / 2;
 
-//points_container.x = app.screen.width / 2;
-//points_container.y = app.screen.height / 2;
-
-//points_container.pivot.x = points_container.width / 2;
-//points_container.pivot.y = points_container.height / 2;
+points_container.pivot.x = points_container.width / 2;
+points_container.pivot.y = points_container.height / 2;
