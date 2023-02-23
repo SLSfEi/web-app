f8ff6ddb852609054b4bf4f988d0c167040a37de
Phodi <5670253+Phodi@users.noreply.github.com>
2023-02-23 18:19:03 +0700

    seperate point scaling operation

--- smart_rplidar/radar_screen/static/js/radar.js
+++ smart_rplidar/radar_screen/static/js/radar.js
@@ -8,20 +8,25 @@ const app = new PIXI.Application(
 const canvas_host = document.getElementById("radar-screen");
 canvas_host.appendChild(app.view);
 
+const max_dist = 6000;
+
 //========== points ==========\\
 const points_container = new PIXI.Container();
 app.stage.addChild(points_container);
 
-// [BUG] Mobile browser not drawing these points
+const scale_point = (x,y) => {
+    const new_x = ((x / max_dist) * 1000) + 500;
+    const new_y = ((y / max_dist) * 1000) + 500;
+    return [new_x,new_y]
+};
+
 const draw_points = (data) =>{
-    const max_dist = 6000;
     while(points_container.children[0]){
         points_container.removeChild(points_container.children[0]);
     }
     for(let i = 0; i < data.length; i++){
         const point_data = data[i];
-        const x = ((point_data.x / max_dist) * 1000) + 500;
-        const y = ((point_data.y / max_dist) * 1000) + 500;
+        const [x,y] = scale_point(point_data.x, point_data.y)
 
         const point = new PIXI.Graphics();
         point.beginFill(0xFF0000)
@@ -46,8 +51,6 @@ for(var i = 0; i < grid_divider*grid_divider; i++){
     point.beginFill(0x000000)
     .drawCircle(0,0,5)
     .endFill()
-    const center_x = app.screen.width / 2;
-    const center_y = app.screen.height / 2;
     point.x = ((i % grid_divider) * grid_distance);
     point.y = (Math.floor(i / grid_divider) * grid_distance);
     grid_container.addChild(point);
