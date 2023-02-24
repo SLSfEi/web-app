56893892b616fa8460cc4a547601bc91f2d47a12
Phodi <5670253+Phodi@users.noreply.github.com>
2023-02-24 20:10:56 +0700

    corrected point scaling

--- smart_rplidar/radar_screen/static/js/radar.js
+++ smart_rplidar/radar_screen/static/js/radar.js
@@ -1,42 +1,16 @@
+//========== init ==========\\
+const app_sides_length = 1000
+const max_dist = 6000;
+
 const app = new PIXI.Application(
     {
-        width: 1000,
-        height: 1000,
+        width: app_sides_length,
+        height: app_sides_length,
         transparent: false,
         background: '#FFFFFF'
     });
 const canvas_host = document.getElementById("radar-screen");
 canvas_host.appendChild(app.view);
-
-const max_dist = 6000;
-
-//========== points ==========\\
-const points_container = new PIXI.Container();
-app.stage.addChild(points_container);
-
-const scale_point = (x,y) => {
-    const new_x = ((x / max_dist) * 1000) + 500;
-    const new_y = ((y / max_dist) * 1000) + 500;
-    return [new_x,new_y]
-};
-
-const draw_points = (data) =>{
-    while(points_container.children[0]){
-        points_container.removeChild(points_container.children[0]);
-    }
-    for(let i = 0; i < data.length; i++){
-        const point_data = data[i];
-        const [x,y] = scale_point(point_data.x, point_data.y)
-
-        const point = new PIXI.Graphics();
-        point.beginFill(0xFF0000)
-        .drawCircle(0,0,10)
-        .endFill();
-        point.x = x;
-        point.y = y;
-        points_container.addChild(point);
-    }
-}
 
 //========== grid =========\\
 const grid_container = new PIXI.Container();
@@ -44,7 +18,6 @@ app.stage.addChild(grid_container);
 const grid_divider = 7;
 
 // dots
-console.log(app.screen.width, app.screen.height)
 const grid_distance = (app.screen.width) / (grid_divider - 1);
 for(var i = 0; i < grid_divider*grid_divider; i++){
     const point = new PIXI.Graphics()
@@ -67,3 +40,32 @@ line.lineStyle(1, 0x000000, 1)
 .moveTo(0, app.screen.height / 2)
 .lineTo(app.screen.width, app.screen.height / 2)
 grid_container.addChild(line)
+
+//========== points ==========\\
+const points_container = new PIXI.Container();
+app.stage.addChild(points_container);
+
+const scale_point = (x,y) => {
+    const new_x = ((x / (max_dist * 2)) * app_sides_length) + (app_sides_length / 2);
+    const new_y = ((y / (max_dist * 2)) * app_sides_length) + (app_sides_length / 2);
+    return [new_x,new_y]
+};
+
+const draw_points = (data) =>{
+    while(points_container.children[0]){
+        points_container.removeChild(points_container.children[0]);
+    }
+    for(let i = 0; i < data.length; i++){
+        const point_data = data[i];
+
+        const [x,y] = scale_point(point_data.x, point_data.y)
+
+        const point = new PIXI.Graphics();
+        point.beginFill(0xFF0000)
+        .drawCircle(0,0,10)
+        .endFill();
+        point.x = x;
+        point.y = y;
+        points_container.addChild(point);
+    }
+}
